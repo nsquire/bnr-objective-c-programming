b33ed037eb7b82195a98b137a735d1db22dd9f39
Nick <user445@server.fake>
2013-07-13 13:28:56 -0700

    Following along in chapter 2.

---
+++ Chapter2/AGoodStart/AGoodStart/main.c
@@ -0,0 +1,22 @@
+//
+//  main.c
+//  AGoodStart
+//
+//  Created by Nicholas Squire on 7/5/13.
+//  Copyright (c) 2013 Nicholas Squire. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+    // Print the beginning of the novel
+    printf("It was the best of times.\n");
+    printf("It was the worst of times.\n");
+    
+    /* Is that actually any good?
+     Maybe it needs a rewrite. */
+    
+    return 0;
+}
+
