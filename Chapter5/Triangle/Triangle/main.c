a7870ebe02262d122853036892e9344fe68ea9d5
Nick <user445@server.fake>
2013-07-21 20:22:47 -0700

    Finishing up challenge from chapter 5.

---
+++ Chapter5/Triangle/Triangle/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  Triangle
+//
+//  Created by Nicholas Squire on 7/21/13.
+//  Copyright (c) 2013 Nicholas Squire. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
