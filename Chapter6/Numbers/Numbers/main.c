68077f782fc3be6ae4c87126888669c4cd36c8fe
Nick <user445@server.fake>
2013-08-04 17:45:02 -0700

    Finishing up chapter 6.

---
+++ Chapter6/Numbers/Numbers/main.c
@@ -0,0 +1,33 @@
+//
+//  main.c
+//  Numbers
+//
+//  Created by Nicholas Squire on 8/4/13.
+//  Copyright (c) 2013 Nicholas Squire. All rights reserved.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, const char * argv[])
+{
+//    int x = 255;
+    
+//    printf("x is %lu.\n", x);
+//    
+//    // Octal and hex already assumed the number was unsigned
+//    printf("In octal, x is %lo.\n", x);
+//    printf("In hexadecimal, x is %lx.\n", x);
+    
+    printf("3 * 3 + 5 * 2 = %d\n", 3 * 3 + 5 *2);
+    printf("11 / 3 = %d remainder of %d \n", 11 / 3, 11 % 3);
+    printf("11 / 3 = %f\n", 11 / (float)3);
+    printf("The absolute value of -5 is %d\n", abs(-5));
+    
+    double y = 12345.6789;
+    printf("y is %.2f\n", y);
+    printf("y is %.2e\n", y);
+    
+    return 0;
+}
+
