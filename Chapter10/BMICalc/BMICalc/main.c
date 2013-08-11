5ea7750068a7639fc47f69f458a64009bd5111bb
Nick <user445@server.fake>
2013-08-11 16:42:40 -0700

    Finishing up challenge from chapter 10.

--- Chapter10/BMICalc/BMICalc/main.c
+++ Chapter10/BMICalc/BMICalc/main.c
@@ -7,6 +7,7 @@
 //
 
 #include <stdio.h>
+#include <time.h>
 
 // Here is the declaration of the struct Person
 typedef struct {
@@ -27,7 +28,21 @@ int main(int argc, const char * argv[])
     printf("person weighs %i kilograms\n", person.weightInKilos);
     printf("person is %.2f meters tall\n", person.heightInMeters);
     float bmi = bodyMassIndex(person);
-    printf("person has a BMI of %.2f", bmi);
+    printf("person has a BMI of %.2f\n", bmi);
+    
+    // Challenge from chapter 10
+    long secondsSince1970 = time(NULL);
+    printf("It has been %ld seconds since 1970\n", secondsSince1970);
+    
+    struct tm now;
+    localtime_r(&secondsSince1970, &now);
+    printf("The time is %d:%d:%d\n", now.tm_hour, now.tm_min, now.tm_sec);
+    
+    long timeInFourMillionSeconds = secondsSince1970 + 4000000;
+    struct tm future;
+    localtime_r(&timeInFourMillionSeconds, &future);
+    printf("The time and date in 4,000,000 seconds is %d:%d:%d %d-%d-20%d \n", future.tm_hour, future.tm_min, future.tm_sec, future.tm_mon + 1, future.tm_mday, future.tm_year - 100);
+    
     return 0;
 }
 
--- Chapter9/PBR/PBR/main.c
+++ Chapter9/PBR/PBR/main.c
@@ -77,4 +77,3 @@ int main(int argc, const char * argv[])
     
     return 0;
 }
-
