7699ea039cdc4868a815e5416b826dc5264108ab
Nick <user445@server.fake>
2013-08-11 12:54:47 -0700

    Finishing up challenge from chapter 8.

--- Chapter8/Addresses/Addresses/main.c
+++ Chapter8/Addresses/Addresses/main.c
@@ -20,6 +20,25 @@ int main(int argc, const char * argv[])
     printf("a pointer is %zu bytes\n", sizeof(addressOfI));
     printf("this function starts at %p\n", main);
     printf("the int stored at addressOfI is %d\n", *addressOfI);
+    
+    // Challenge for chapter 8
+    printf("a float is %zu bytes\n", sizeof(float));
+    printf("a short is %zu bytes\n", sizeof(short));
+    
+    short j = 0;
+    printf("the value of j is %d\n", j);
+    j = 32799; // wraps around after maxing out
+    printf("now the value of j is %d\n", j);
+    j = 32767; // max value
+    printf("now the value of j is %d\n", j);
+    
+    unsigned short k = 0;
+    printf("the value of k is %d\n", k);
+    k = 65560;
+    printf("now the value of k is %hu\n", k);
+    k = 65535; // max value
+    printf("now the value of k is %d\n", k);
+    
     return 0;
 }
 
