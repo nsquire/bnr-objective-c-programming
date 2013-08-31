d68dd57de834309f69b7112919bf93ade0a027b4
Nick <user445@server.fake>
2013-08-31 15:01:43 -0700

    Following along in chapter 26.

---
+++ Chapter26/Stockz/Stockz/main.m
@@ -0,0 +1,49 @@
+//
+//  main.m
+//  Stockz
+//
+//  Created by Nicholas Squire on 8/31/13.
+//  Copyright (c) 2013 Nicholas Squire. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        NSMutableArray *stocks = [[NSMutableArray alloc] init];
+        NSMutableDictionary *stock;
+        
+        stock = [NSMutableDictionary dictionary];
+        [stock setObject:@"AAPL"
+                  forKey:@"symbol"];
+        [stock setObject:[NSNumber numberWithInt:200]
+                  forKey:@"shares"];
+        
+        [stocks addObject:stock];
+        
+        stock = [NSMutableDictionary dictionary];
+        [stock setObject:@"GOOG"
+                  forKey:@"symbol"];
+        [stock setObject:[NSNumber numberWithInt:160]
+                  forKey:@"shares"];
+        
+        [stocks addObject:stock];
+        
+        [stocks writeToFile:@"/tmp/stocks.plist"
+                 atomically:YES];
+        
+        NSArray *stockList = [NSArray arrayWithContentsOfFile:@"/tmp/stocks.plist"];
+        
+        for (NSDictionary *d in stockList) {
+            NSLog(@"I have %@ shares of %@",
+                  [d objectForKey:@"shares"],
+                  [d objectForKey:@"symbol"]);
+        }
+        
+    }
+    return 0;
+}
+
